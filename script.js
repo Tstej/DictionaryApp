db83b539431e03971eed8ed78db83db925801d0f
Tejal Shinde <tejal.shinde785@gmail.com>
2024-10-23 11:46:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const form = document.querySelector("form");
+const resultDiv = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWordInfo(form.elements[0].value);
+});
+
+const getWordInfo = async (word) => {
+  resultDiv.innerHTML=`<p>Finding Word....</p>`
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    const data = await response.json();
+
+    let definitions = data[0].meanings[0].definitions[0];
+
+    resultDiv.innerHTML = `
+        <h2><strong>Word: </strong> ${data[0].word}</h2>
+        <p class="partOfSpeech">${data[0].meanings[0].partOfSpeech}</p>
+        <p><strong>Definition: </strong> ${definitions.definition}</p>
+        <p><strong>Example: </strong> ${
+          definitions.example === undefined
+            ? "Not Found"
+            : data[0].meanings[0].definitions[0].example
+        }</p>
+    `;
+
+    // To fetch Antonyms Array values
+    resultDiv.innerHTML += ` <p><strong>Antonyms: </strong></p>`;
+
+    if (definitions.antonyms.length === 0) {
+      resultDiv.innerHTML += `<span>Not Found</span> <br>`;
+    } else {
+      for (let i = 0; i < definitions.antonyms.length; i++) {
+        resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`;
+      }
+    }
+
+    // To fetch Synonyms Array values
+    resultDiv.innerHTML += `<p><strong>Synonyms: </strong></p>`;
+
+    if (definitions.synonyms.length === 0) {
+      resultDiv.innerHTML += `<span>Not Found</span>`;
+    } else {
+      for (let i = 0; i < definitions.synonyms.length; i++) {
+        resultDiv.innerHTML += `<li>${definitions.synonyms[i]}</li>`;
+      }
+    }
+
+    resultDiv.innerHTML += `<div><a href="${data[0].sourceUrls}" target="_blank">Read More</a></div>`;
+  } catch (error) {
+    resultDiv.innerHTML = `<p>Sorry, the word could not be found!</p>`;
+  }
+};
